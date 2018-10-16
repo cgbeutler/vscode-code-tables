c8c63450766784020c8771060ebefe4a77551d33
Cory Beutler <cg.beutler@gmail.com>
2018-10-16 17:46:16 -0600

    Got tables to view correctly

--- src/extension.ts
+++ src/extension.ts
@@ -7,21 +7,77 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "code-tables" is now active!');
+	console.log('decorator sample is activated');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+	// create a decorator type that we use to decorate large numbers
+	const tabDecorationType = vscode.window.createTextEditorDecorationType({
+        letterSpacing: '-1em',
+        opacity: '0'
+	});
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+	let activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
 
-    context.subscriptions.push(disposable);
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	var timeout: NodeJS.Timer | null = null;
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+        }
+        const tabLineRegEx = /(?:.*\t.*(?:\r?\n|$))+/g;
+		const text = activeEditor.document.getText();
+        const tabLines: vscode.DecorationOptions[] = [];
+        let match;
+        while (match = tabLineRegEx.exec(text)) {
+            var lineCols = match[0].replace(/\r/g, "").split("\n").filter(l => l.length > 0)
+                .map(l => l.split("\t"));
+            var colLines = lineCols[0].map((_, i) => lineCols.map(row => row[i]));
+            var colSizes = colLines.map(col => col.reduce((p, c) => p.length > c.length ? p : c).length);
+
+            const tabOrNewLineRegEx = /(?:(.*?)(\t|\n|\r\n))/g;
+            let subMatch;
+            let currentColumn = 0;
+            while (subMatch = tabOrNewLineRegEx.exec(match[0])) {
+                if (subMatch[2] === "\n" || subMatch[2] === "\r\n") {
+                    currentColumn = 0;
+                } else {
+                    var padding = "_".repeat(colSizes[currentColumn] - subMatch[1].length);
+                    var tabIndex = match.index + subMatch.index + subMatch[1].length;
+                    var startPos = activeEditor.document.positionAt(tabIndex);
+                    var endPos = activeEditor.document.positionAt(tabIndex + 1);
+                    tabLines.push({
+                        range: new vscode.Range(startPos, endPos),
+                        renderOptions: {
+                            after: {contentText: padding + "|", color: "rgba(127,127,127,0.0)"},
+                        }
+                    });
+                    currentColumn++;
+                }
+            }
+        }
+		activeEditor.setDecorations(tabDecorationType, tabLines);
+	}
 }
 
 // this method is called when your extension is deactivated
